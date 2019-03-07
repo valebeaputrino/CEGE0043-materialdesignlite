72f991ea4bb1e59f67db2f665a3b7d774dcf417b
valebeaputrino <v.putrino@ucl.ac.uk>
2019-03-07 07:22:32 -0500

    new piece of code for the logo

--- ucesvpu/platforms/browser/www/js/rhMenus.js
+++ ucesvpu/platforms/browser/www/js/rhMenus.js
@@ -1,5 +1,8 @@
 function menuClicked(){
-	alert("You clicked the menu")
-}function addUCLLogo(){
-	document.getElementByld("ucllogo").innerHTML ="<img src=' images/ucl.png'>"
+	alert("You clicked the menu");
+}
+
+function addUCLLogo(){
+	alert("ucllogo clicked");
+	document.getElementById("ucllogo").innerHTML="<img src='images/ucl.png'>"
 }
